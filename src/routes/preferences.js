f20b2aad37814758c997dfef5e01644e1ee50783
Pablo Chuquimia <i.pouk.19@gmail.com>
2025-12-28 14:40:21 -0400

    fixing thme color

--- src/routes/preferences.js
+++ src/routes/preferences.js
@@ -6,21 +6,26 @@ const router = Router()
 router.get('/', async (req, res) => {
   const userId = req.query.userId || 'default'
   const pref = await Preference.findOne({ userId }).lean()
+  res.set('Cache-Control', 'no-store')
   res.json(pref || { userId, branch: 'general', goals: {} })
 })
 
 router.post('/', async (req, res) => {
   const userId = req.body.userId || 'default'
   const update = {}
-  if (req.body.branch) update.branch = req.body.branch
-  if (req.body.goals) update.goals = req.body.goals
-  if (!update.branch) update.branch = 'general'
+  if (Object.prototype.hasOwnProperty.call(req.body, 'branch')) {
+    update.branch = req.body.branch || 'general'
+  }
+  if (Object.prototype.hasOwnProperty.call(req.body, 'goals')) {
+    update.goals = req.body.goals || {}
+  }
 
   const pref = await Preference.findOneAndUpdate(
     { userId },
     { $set: update },
     { new: true, upsert: true, setDefaultsOnInsert: true },
   )
+  res.set('Cache-Control', 'no-store')
   res.status(201).json(pref)
 })
 
